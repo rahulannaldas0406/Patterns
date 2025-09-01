794a8aca6f27e84a57388498489b0451a0bd49b1
Annaldas Rahul <rahulannaldas2006@gmail.com>
2025-09-01 18:31:05 +0530

    Using two and three for loops

---
+++ day1.py
@@ -0,0 +1,94 @@
+#Square of *
+
+#test cases 
+'''n=4
+
+****
+****
+****
+****'''
+'''n=4
+print((("*"*n +"\n")*n).rstrip())'''
+
+#code 
+
+'''for i in range(0,4):
+    for j in range(0,4):
+        print("*",end="")
+    print()'''
+
+#using while loop
+
+'''i,j=0,0
+
+while i<4:
+    while j<4:
+        print("*",end="")
+        j+=1
+    print()
+    i+=1
+    j=0'''
+
+#square of numbers (1s, 2s, etc.)
+
+'''n=5
+11111
+22222
+33333
+44444
+55555'''
+
+'''for i in range(1,6):
+    for j in range(1,6):
+        print(i,end="")
+    print()'''
+
+#using while loop
+
+'''i,j=1,1
+while i<6:
+    while j<6:
+        print(i,end="")
+        j+=1
+    print()
+    i+=1
+    j=1'''
+
+#Square of Increasing Numbers
+
+'''n=3
+123
+123
+123'''
+
+'''for i in range(1,4):
+    for j in range(1,4):
+        print(j,end="")
+    print()'''
+
+'''i,j=1,1
+while i<4:
+    while j<4:
+        print(j,end="")
+        j+=1
+    print()
+    j=1
+    i+=1'''
+
+#Square of alphabets (A, B, etc.)
+
+'''n=3
+ABC
+ABC
+ABC'''
+
+# a=chr(65)
+# print()
+
+for i in range(0,3):
+    for j in range(65,68):
+        print(chr(j),end="")
+    print()
+
+
+
