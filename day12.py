dc9d581db45fde3f00ff095f11bd438ded4184a4
Annaldas Rahul <rahulannaldas2006@gmail.com>
2025-10-08 19:18:58 +0530

    update code

--- day12.py
+++ day12.py
@@ -1,6 +1,6 @@
 #Alternating 0 and 1 in rows
 
-n=5
+#n=6
 
 '''
 1 0 1 0 1
@@ -36,7 +36,7 @@ n=5
 0 1 0 1
 '''
 
-m=4
+'''m=4
 for i in range(0,n):
     if i%2==0:
         val=0
@@ -47,4 +47,35 @@ for i in range(0,n):
         val=1-val
     
 
+    print()'''
+
+#Diamond of numbers
+
+n=6
+
+'''
+    1
+   1 2
+  1 2 3
+ 1 2 3 4
+1 2 3 4 5
+ 1 2 3 4
+  1 2 3
+   1 2
+    1
+'''
+
+for i in range(1,n*2):
+    if i<=n:
+        for s in range(0,n-i):
+            print(" ",end="")
+        for j in range(1,i+1):
+            print(j,end=" ")
+    else:
+        for s in range(0,i-n):
+            print(" ",end="")
+        for j in range(1,n*2-i+1):
+            print(j,end=" ")
+
     print()
+    
